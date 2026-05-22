194348b94c9932603c08d59f1d5c4b74b9d9b71e
Tifelabs <boluwatifeolusegun018@gmail.com>
2026-05-21 18:17:20 -0600

    Expanding Bits Reps

---
+++ chap 2/expansion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len){
+    for(int i = 0; i < len; i++){
+        printf(" %.2x",start[i]);
+    }
+    printf("\n");
+}
+
+
+int main(){
+    short sx           = -12345;   /* -12345 */
+    unsigned short usx = sx;       /* 53191  */
+    int x              = sx;       /* -12345 */
+    unsigned ux        = usx;      /* 53191  */
+
+    printf("sx = %d:\t", sx);
+    show_bytes((byte_pointer) &sx, sizeof(short));
+
+    printf("usx = %d:\t", usx);
+    show_bytes((byte_pointer) &usx, sizeof(unsigned short));
+
+    printf("x   = %d:\t", x);
+    show_bytes((byte_pointer) &x, sizeof(int));
+
+    printf("ux  = %u:\t",ux);
+    show_bytes((byte_pointer) &ux, sizeof(unsigned));
+}
